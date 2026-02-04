a25d4420842889fa750c826cf8cb189edee5d1c3
shubhamgupta8899 <shubhamkrgupta2026@gmail.com>
2026-02-05 01:45:32 +0530

    make shipping component in CheckoutPage...(Some Changes to need to do in this part)...

--- shoppingCart/src/pages/Cart.jsx
+++ shoppingCart/src/pages/Cart.jsx
@@ -1,72 +1,99 @@
-import React from 'react'
-import { useCart } from '../context/CartContext';
-import { Link } from 'react-router-dom';
-import {ShoppingCart, ChevronLeft, Zap} from 'lucide-react'
-import CartItem from '../components/CartItem';
+import React from "react";
+import { useCart } from "../context/CartContext";
+import { Link } from "react-router-dom";
+import { ShoppingCart, ChevronLeft, Zap } from "lucide-react";
+import CartItem from "../components/CartItem";
 
 const Cart = () => {
-
-  const {cart, cartTotalPrice, cartCount} = useCart();
+  const { cart, cartTotalPrice, cartCount } = useCart();
 
   return (
     <>
-      <div className='container max-w-7xl  mx-auto px-4 md:px-8 pt-8  '>
-        <div className='flex items-center mb-10'>
-          <Link to={'/'} className='flex items-center text-gray-400 hover:text-orange-400 transition
-          duration-150 font-semibold text-lg'>
-            <ChevronLeft className='w-6 h-6 mr-1' />
+      <div className="container max-w-7xl  mx-auto px-4 md:px-8 pt-8  ">
+        <div className="flex items-center mb-10">
+          <Link
+            to={"/"}
+            className="flex items-center text-gray-400 hover:text-orange-400 transition
+          duration-150 font-semibold text-lg"
+          >
+            <ChevronLeft className="w-6 h-6 mr-1" />
             <span>Back to Home</span>
           </Link>
         </div>
 
-        <h2 className=' text-2xl md:text-3xl lg:text-4xl font-bold md:font-extrabold lg:font-extrabold text-white
-        mb-10 tracking-tight'>Shopping Cart({cartCount}) </h2>
+        <h2
+          className=" text-2xl md:text-3xl lg:text-4xl font-bold md:font-extrabold lg:font-extrabold text-white
+        mb-10 tracking-tight"
+        >
+          Shopping Cart({cartCount}){" "}
+        </h2>
 
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 '>
-          <div className='lg:col-span-2 space-y-4'>
-            {cart.map(item=> <CartItem key={item.id} item={item} />)}
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 ">
+          <div className="lg:col-span-2 space-y-4">
+            {cart.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
           </div>
 
-          <div className='lg:col-span-1  p-8 bg-gray-900 rounded-2xl shadow-2xl sticky top-20
-          lg:sticky h-fit border border-gray-800'>
-
-            <h3 className='lg:text-3xl md:text-2xl sm:text-xl font-bold mb-5 border-b border-y-gray-700 pb-3 flex 
-            items-center space-x-2'>
-              <div className='flex justify-between'>
-                <span className='w-6 h-6 text-orange-400'>₹</span>
+          <div
+            className="lg:col-span-1  p-8 bg-gray-900 rounded-2xl shadow-2xl sticky top-20
+          lg:sticky h-fit border border-gray-800"
+          >
+            <h3
+              className="lg:text-3xl md:text-2xl sm:text-xl font-bold mb-5 border-b border-y-gray-700 pb-3 flex 
+            items-center space-x-2"
+            >
+              <div className="flex justify-between">
+                <span className="w-6 h-6 text-orange-400">₹</span>
                 <span>Order Total</span>
               </div>
             </h3>
 
-            <div className='space-y-4 text-gray-400'>
-              <div className='flex justify-between text-xl '>
-                <span className='sm:text-sm lg:text-xl md:text-xl'>SubTotal :</span>
-                <span className='font-semibold sm:text-sm lg:text-xl md:text-xl text-white'>₹{cartTotalPrice.toFixed(2)}</span>
+            <div className="space-y-4 text-gray-400">
+              <div className="flex justify-between text-xl ">
+                <span className="sm:text-sm lg:text-xl md:text-xl">
+                  SubTotal :
+                </span>
+                <span className="font-semibold sm:text-sm lg:text-xl md:text-xl text-white">
+                  ₹{cartTotalPrice.toFixed(2)}
+                </span>
               </div>
 
-              <div className='flex justify-between text-xl '>
-                <span className='sm:text-sm lg:text-xl md:text-xl'>Shipping-Cost :</span>
-                <span className='font-semibold text-green-400 sm:text-sm lg:text-xl md:text-xl'>Free</span>
+              <div className="flex justify-between text-xl ">
+                <span className="sm:text-sm lg:text-xl md:text-xl">
+                  Shipping-Cost :
+                </span>
+                <span className="font-semibold text-green-400 sm:text-sm lg:text-xl md:text-xl">
+                  Free
+                </span>
               </div>
 
-              <div className='flex justify-between pt-6 border-t border-gray-700 '>
-                <span className='sm:text-sm lg:text-xl md:text-xl font-bold text-white'>Estimated Total :</span>
-                <span className='sm:text-sm lg:text-xl md:text-xl font-bold text-orange-400'>₹{cartTotalPrice.toFixed(2)}</span>
+              <div className="flex justify-between pt-6 border-t border-gray-700 ">
+                <span className="sm:text-sm lg:text-xl md:text-xl font-bold text-white">
+                  Estimated Total :
+                </span>
+                <span className="sm:text-sm lg:text-xl md:text-xl font-bold text-orange-400">
+                  ₹{cartTotalPrice.toFixed(2)}
+                </span>
               </div>
             </div>
-            
-            <Link to={'/checkout'} className=' py-4 flex space-x-2 bg-orange-600 rounded-full mt-8 items-center justify-center
-            hover:bg-orange-600/50 shadow-orange-800/50 shadow-lg transform uppercase hover:ring-4 hover:ring-orange-900/50'>
-            
-            <Zap className='w-6 h-6'/>
-            <span>Proceed Securely</span>
+
+            <Link
+              to={"/checkout"}
+              className=" py-4 flex space-x-2 bg-orange-600 rounded-full mt-8 items-center justify-center
+            hover:bg-orange-600/50 shadow-orange-800/50 shadow-lg transform uppercase hover:ring-4 hover:ring-orange-900/50"
+            >
+              <Zap className="w-6 h-6" />
+              <span>Proceed Securely</span>
             </Link>
-            <p className='text-xs text-gray-500 text-center mt-4'>All transaction are encrypted and Secure.</p>
+            <p className="text-xs text-gray-500 text-center mt-4">
+              All transaction are encrypted and Secure.
+            </p>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Cart;
--- shoppingCart/src/pages/CheckOut.jsx
+++ shoppingCart/src/pages/CheckOut.jsx
@@ -1,17 +1,71 @@
-import React, { useState } from 'react'
-import { useCart } from '../context/CartContext';
+import React, { useState } from "react";
+import { useCart } from "../context/CartContext";
 
-import {Package, MapPin} from 'lucide-react'
+import { Package, MapPin } from "lucide-react";
 const CheckOut = () => {
-
-  useState
+  const {cartTotalPrice} = useCart();
+  const [deliveryDetails, setDeliveryDetails] = useState({
+    name: "",
+    address: "",
+    city: "",
+    zip: "",
+  });
   return (
-    <div>
-      <div>
+    <div className="container max-w-7xl mx-auto px-4 md:px-8 pt-8">
+      <h2 className="text-xl md:text-2xl lg:text-3xl font-extrabold text-white mb-10 tracking-tight ">
+        Finilize Order
+      </h2>
 
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+        <div
+          className="lg:col-span-2 p-8 bg-gray-900 rounded-2xl shadow-2xl border border-gray-800
+        "
+        >
+          <h3
+            className="text-3xl font-bold text-orange-400 mb-6 flex items-center space-x-3 border-b 
+          border border-gray-700 pb-4 "
+          >
+            <MapPin className="w-7 h-7 text-orange-400" />
+            <span>Shipping Information</span>
+          </h3>
+
+          <form className="space-y-6 ">
+            {Object.keys(deliveryDetails).map((key) => (
+              <div key={key}>
+                <label
+                  htmlFor={key}
+                  className="block text-sm font-semibold text-gray-300 capitalize mb-1"
+                >
+                  {key == "zip" ? "pin Code" : key}
+                </label>
+
+                <input text={key === "zip" ? "number":"text"}
+                id={key}
+                name={key}
+                value={deliveryDetails[key]}
+                required
+                className="mt-1 block w-full px-5 py-3 border border-gray-700 rounded-xl shadow-inner text-white bg-gray-800
+                placeholder-gray-500"
+                />
+              </div>
+            ))}
+
+            <div className="pt-6">
+
+              <button
+              type="submit"
+              className=" w-full py-4 flex space-x-2 text-xl font-bold cursor-pointer bg-orange-600 rounded-full items-center justify-center
+            hover:bg-orange-600/50 shadow-orange-800/50 shadow-lg transform uppercase hover:ring-4 hover:ring-orange-900/50"
+            >
+              {/* <Zap className="w-6 h-6" /> */}
+              <span>₹Pay and Confirm Order(₹{cartTotalPrice.toFixed(2)})</span>
+            </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CheckOut;
